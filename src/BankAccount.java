701de10a1c56f4dbda9005ae39e0b0702c305936
Storrm01 <81132531+Storrm01@users.noreply.github.com>
2025-01-29 19:39:46 +0000

    Final Commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,13 @@
-
+public class BankAccount{
+    public static void main(String[] ags){
+        double balance = 2175.37;
+        balance -= 302.50;
+        balance += 50.03;
+        balance /= 2;
+        balance += 20.00;
+        balance -= 1;
+        balance *= 2;
+        balance += 1;
+        System.out.println(String.format("%.2f", balance));
+    }
+}
